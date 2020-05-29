c824e42bb891b12a49fb3a6513b26d63a46be559
kenendo12 <kenendo12@gmail.com>
2020-05-29 12:39:40 -0500

    adds completed view model and business logic for cookbook controller

--- CookingPapa/CookingPapa.Domain/Business/BusinessL.cs
+++ CookingPapa/CookingPapa.Domain/Business/BusinessL.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,7 +18,13 @@ namespace CookingPapa.Domain.Business
         {
             _unitOfWork = unitOfWork;
         }
-        
+        /// <summary>
+        /// Saves a recipe into a user cookbook.
+        /// If the recipe already exist in the cook book, the checkIfExists will return an instance of
+        /// Cookbook and return null/error to the user.
+        /// </summary>
+        /// <param name="cookbook">includes user Id and recipe Id to save to cookbook</param>
+        /// <returns></returns>
         public async Task<Cookbook> PostCookbook(PostCookbookVM cookbook)
         {
             var checkIfExists = _unitOfWork.Cookbooks.GetByUserEager(cookbook.UserId)
@@ -30,6 +37,36 @@ namespace CookingPapa.Domain.Business
                 _unitOfWork.Cookbooks.Add(newCookbook);
                 await _unitOfWork.Complete();
                 return newCookbook;
+            }
+            return null;
+        }
+        /// <summary>
+        /// pulls list of recipe under a cookbook and organize it into an instance of GetcookbookVM
+        /// to return information client needs.
+        /// </summary>
+        /// <param name="id">User id</param>
+        /// <returns></returns>
+        public async Task<List<GetCookbookVM>> GetCookbook(int id)
+        {
+            //return the specific cook book with the list of recipes.
+            var cookbook = _unitOfWork.Cookbooks.GetByUserEager(id).Result.ToList();
+            if (cookbook != null)
+            {
+                List<GetCookbookVM> newCookbookList = new List<GetCookbookVM>();
+                foreach (var x in cookbook)
+                {
+                    newCookbookList.Add(new GetCookbookVM()
+                    {
+                        RecipeId = x.Recipe.Id,
+                        UserId = x.User.Id,
+                        CookbookId = x.Id,
+                        RecipeName = x.Recipe.RecipeName,
+                        RecipeOrigin = x.Recipe.RecipeOrigin.RecipeOriginName,
+                        RecipeCookTime = x.Recipe.RecipeCookTime
+                    }
+                    );
+                }
+                return newCookbookList;
             }
             return null;
         }
