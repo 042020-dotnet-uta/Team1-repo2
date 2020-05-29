2578b63e89dafac5aac20c0fe1e702e7ca0a0332
RyanShereda <39539590+RyanShereda@users.noreply.github.com>
2020-05-28 23:12:40 -0500

    Adds documentation to repository interface files.

--- CookingPapa/CookingPapa.Domain/RepositoryInterfaces/IRecipeIngredientGroupRepository.cs
+++ CookingPapa/CookingPapa.Domain/RepositoryInterfaces/IRecipeIngredientGroupRepository.cs
@@ -6,10 +6,29 @@ using System.Threading.Tasks;
 
 namespace CookingPapa.Domain.RepositoryInterfaces
 {
+    /// <summary>
+    /// Interface for the RecipeIngredientGroup-specific repository.
+    /// </summary>
     public interface IRecipeIngredientGroupRepository : IRepository<RecipeIngredientGroups>
     {
+        /// <summary>
+        /// Delete the entry at the given ID.
+        /// </summary>
+        /// <param name="id">The ID of the entity to be deleted.</param>
         void Delete(int id);
+
+        /// <summary>
+        /// Eager-loads the entity at the given ID.
+        /// </summary>
+        /// <param name="id">The ID of the entity to be fetched.</param>
+        /// <returns>Returns the entity with the given ID.</returns>
         Task<RecipeIngredientGroups> GetEager(int id);
+
+        /// <summary>
+        /// Fetches all entities with the given Recipe ID.
+        /// </summary>
+        /// <param name="id">The Recipe ID to filter by.</param>
+        /// <returns>Returns an IEnumerable populated with the appropriate entities.</returns>
         Task<IEnumerable<RecipeIngredientGroups>> GetByRecipeEager(int id);
     }
 }
--- CookingPapa/CookingPapa.Domain/RepositoryInterfaces/IReviewRepository.cs
+++ CookingPapa/CookingPapa.Domain/RepositoryInterfaces/IReviewRepository.cs
@@ -6,10 +6,29 @@ using System.Threading.Tasks;
 
 namespace CookingPapa.Domain.RepositoryInterfaces
 {
+    /// <summary>
+    /// Interface for the RecipeReview-specific repository.
+    /// </summary>
     public interface IReviewRepository : IRepository<RecipeReview>
     {
+        /// <summary>
+        /// Delete the entry at the given ID.
+        /// </summary>
+        /// <param name="id">The ID of the entity to be deleted.</param>
         void Delete(int id);
+
+        /// <summary>
+        /// Eager-loads the entity at the given ID.
+        /// </summary>
+        /// <param name="id">The ID of the entity to be fetched.</param>
+        /// <returns>Returns the entity with the given ID.</returns>
         Task<RecipeReview> GetEager(int id);
+
+        /// <summary>
+        /// Fetches all entities with the given Recipe ID.
+        /// </summary>
+        /// <param name="id">The Recipe ID to filter by.</param>
+        /// <returns>Returns an IEnumerable populated with the appropriate entities.</returns>
         Task<IEnumerable<RecipeReview>> GetByRecipeEager(int id);
     }
 }
